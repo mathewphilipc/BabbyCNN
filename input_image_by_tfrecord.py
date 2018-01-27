0f29533a8a5655fb3dbee64a565d480870a4c496
Mathew Calkins <mathewpcalkins@gmail.com>
2018-01-27 18:26:34 -0500

    more playing around with image reading

--- input_image_by_tfrecord.py
+++ input_image_by_tfrecord.py
@@ -1,3 +1,30 @@
 import os
 import tensorflow as tf
+import PIL
 
+# define a function to list tfrecord files.
+
+def list_tfrecord_file(file_list):
+	tfrecord_list = []
+	for i in range(len(file_list)):
+		current_file_abs_path = os.path.abspath(file_list[i])
+		if current_file_abs_path.endswirth(".tfrecord"):
+			tfrecord_list.append(current_file_abs_path)
+			print("found %s successfully!" % file_list[i])
+		else:
+			pass
+	return tfrecord_list
+
+# Traverse current directory
+def tfrecord_auto_traversal():
+	PATH = "/home/mathew/Desktop/NWPU-RESISC45/"
+	current_folder_filename_list = os.listdir(PATH) # Change this PATH to traverse other directories if you want.
+	print(current_folder_filename_list)
+#	print("hellooo")
+#	return 5
+
+def main():
+	print(os.listdir("/home/mathew/Desktop/NWPU-RESISC45/"))
+
+if __name__ == "__main__":
+	main()

7c8a75de9564f3ed4692b6d57a149cf985ef9fa0
Mathew Calkins <mathewpcalkins@gmail.com>
2018-02-07 11:50:16 -0500

    benchmarking

--- AlexNet_Test.py
+++ AlexNet_Test.py
@@ -20,8 +20,8 @@ N_CLASSES = FULL_N_CLASSES
 
 print(N_CLASSES)
 
-IMG_HEIGHT = 256 # original size = 256
-IMG_WIDTH = 256 # original size = 256
+IMG_HEIGHT = 64 # original size = 256
+IMG_WIDTH = 64 # original size = 256
 CHANNELS = 3 # we have full-color images
 
 
